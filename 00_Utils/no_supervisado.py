0f96c647b90e8afc16013beb0dc5c92e69105e70
María DM <mariad1996@gmail.com>
2024-09-26 19:15:43 +0200

    S15 U2 update

--- 00_Utils/no_supervisado.py
+++ 00_Utils/no_supervisado.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FixedLocator, FixedFormatter
 import numpy as np
 
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, DBSCAN
 from sklearn.metrics import silhouette_score, silhouette_samples
+
+
+# KMeans
 
 X = 'array'
 k = 5
@@ -72,3 +75,69 @@ for k in (3, 4, 5, 6):
     plt.title('$k={}$'.format(k), fontsize = 16)
 
 plt.show()
+
+def plot_data(X):
+    plt.plot(X[:, 0], X[:, 1], 'k.', markersize = 2)
+
+
+def plot_centroids(centroids, weights = None, circle_color = 'w', cross_color = 'b'):
+    if weights is not None:
+        centroids  =  centroids[weights > weights.max() / 10]
+        
+    plt.scatter(centroids[:, 0], centroids[:, 1], marker = 'o', s = 30, linewidths = 8, color = circle_color, zorder = 10, alpha = 0.9)
+    plt.scatter(centroids[:, 0], centroids[:, 1], marker = 'x', s = 15, linewidths = 20, color = cross_color, zorder = 11, alpha = 1)
+
+
+def plot_decision_boundaries(clusterer, X, resolution = 1000, show_centroids = True, show_xlabels = True, show_ylabels = True):
+    mins = X.min(axis = 0) - 0.1
+    maxs = X.max(axis = 0) + 0.1
+    xx, yy = np.meshgrid(np.linspace(mins[0], maxs[0], resolution), np.linspace(mins[1], maxs[1], resolution))
+    Z = clusterer.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.contourf(Z, extent = (mins[0], maxs[0], mins[1], maxs[1]), cmap = 'Pastel2')
+    plt.contour(Z, extent = (mins[0], maxs[0], mins[1], maxs[1]), linewidths = 1, colors = 'k')
+    plot_data(X)
+    
+    if show_centroids:
+        plot_centroids(clusterer.cluster_centers_)
+
+    if show_xlabels:
+        plt.xlabel('$x_1$', fontsize = 14)
+    else:
+        plt.tick_params(labelbottom = False)
+        
+    if show_ylabels:
+        plt.ylabel('$x_2$', fontsize = 14, rotation = 0)
+    else:
+        plt.tick_params(labelleft = False)
+        
+        
+# DBSCAN
+
+def plot_dbscan(dbscan, X, size, show_xlabels = True, show_ylabels = True):
+    core_mask = np.zeros_like(dbscan.labels_, dtype = bool)
+    core_mask[dbscan.core_sample_indices_] = True
+    anomalies_mask = dbscan.labels_ == -1
+    non_core_mask = ~(core_mask | anomalies_mask)
+
+    cores = dbscan.components_
+    anomalies = X[anomalies_mask]
+    non_cores = X[non_core_mask]
+    
+    plt.scatter(cores[:, 0], cores[:, 1], c = dbscan.labels_[core_mask], marker = 'o', s = size, cmap = 'Paired')
+    plt.scatter(cores[:, 0], cores[:, 1], marker = '*', s = 20, c = dbscan.labels_[core_mask])
+    plt.scatter(anomalies[:, 0], anomalies[:, 1], c = 'r', marker = 'x', s = 100)
+    plt.scatter(non_cores[:, 0], non_cores[:, 1], c = dbscan.labels_[non_core_mask], marker = '.')
+    
+    if show_xlabels:
+        plt.xlabel('$x_1$', fontsize = 14)
+    else:
+        plt.tick_params(labelbottom = False)
+        
+    if show_ylabels:
+        plt.ylabel('$x_2$', fontsize = 14, rotation = 0)
+    else:
+        plt.tick_params(labelleft = False)
+        
+    plt.title('eps = {:.2f}, min_samples = {}'.format(dbscan.eps, dbscan.min_samples), fontsize = 14)
