565e5ab5828041c89e51aceea4034918fffcded2
María DM <mariad1996@gmail.com>
2024-11-30 13:46:40 +0100

    S24 update

--- 06_Data_Engineering/Sprint_24/Taller_pythonanywhere/app_model_hooks.py
+++ 06_Data_Engineering/Sprint_24/Taller_pythonanywhere/app_model_hooks.py
@@ -16,13 +16,13 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # Enruta la landing page (endpoint /)
-@app.route('/', methods=['GET'])
+@app.route('/', methods = ['GET'])
 def hello():
     return "Bienvenido a mi API del modelo advertising"
 
 # Enruta la funcion al endpoint /api/v1/predict
 
-@app.route('/api/v1/predict', methods=['GET'])
+@app.route('/api/v1/predict', methods = ['GET'])
 def predict(): # Ligado al endpoint '/api/v1/predict', con el método GET
 
     model = pickle.load(open(path_base + 'ad_model.pkl','rb'))
@@ -30,32 +30,29 @@ def predict(): # Ligado al endpoint '/api/v1/predict', con el método GET
     radio = request.args.get('radio', None)
     newspaper = request.args.get('newspaper', None)
 
-    print(tv,radio,newspaper)
+    print(tv, radio, newspaper)
     print(type(tv))
 
     if tv is None or radio is None or newspaper is None:
-        return "Args empty, the data are not enough to predict, STUPID!!!!"
+        return 'Args empty, the data are not enough to predict, STUPID!!!!'
     else:
         prediction = model.predict([[float(tv),float(radio),float(newspaper)]])
     
     return jsonify({'predictions': prediction[0]})
 
-@app.route('/api/v1/retrain', methods=['GET'])
+@app.route('/api/v1/retrain', methods = ['GET'])
 # Enruta la funcion al endpoint /api/v1/retrain
 def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
-    if os.path.exists(path_base + "data/Advertising_new.csv"):
+    if os.path.exists(path_base + 'data/Advertising_new.csv'):
         data = pd.read_csv(path_base + 'data/Advertising_new.csv')
 
-        X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['sales']),
-                                                        data['sales'],
-                                                        test_size = 0.20,
-                                                        random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(data.drop(columns = ['sales']), data['sales'], test_size = 0.20, random_state = 42)
 
-        model = Lasso(alpha=6000)
+        model = Lasso(alpha = 6000)
         model.fit(X_train, y_train)
         rmse = np.sqrt(mean_squared_error(y_test, model.predict(X_test)))
         mape = mean_absolute_percentage_error(y_test, model.predict(X_test))
-        model.fit(data.drop(columns=['sales']), data['sales'])
+        model.fit(data.drop(columns = ['sales']), data['sales'])
         pickle.dump(model, open(path_base + 'ad_model.pkl', 'wb'))
 
         return f"Model retrained. New evaluation metric RMSE: {str(rmse)}, MAPE: {str(mape)}"
